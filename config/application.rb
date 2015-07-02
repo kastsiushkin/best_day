9066b968c3a22b50c04aaee7ac2b451f694c2c88
kastsiushkin <alexkastsiushkin@gmail.com>
2015-07-01 19:40:25 -0700

    Add config.serve_static_assets = true

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module BestDay
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.serve_static_files = true
+    config.serve_static_assets = true
 
     config.assets.paths << Rails.root.join('app', 'assets', 'images')
 
